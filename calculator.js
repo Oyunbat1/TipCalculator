067d22640a2017941ca01f8b2b345f86d7e55117
Oyunbat <oyunbat9958@gmail.com>
2025-01-21 09:50:56 -0600

    event listener ashiglasan code done

--- calculator.js
+++ calculator.js
@@ -1,57 +1,81 @@
-function fivePersent() {
-    const getInput = document.getElementById("input-bill").value; //x
-    const finalOutput = (getInput * 5) / 100;
-    document.getElementById("num").innerHTML = finalOutput;
-    document.getElementById('input-bill').value = '0';
+// function fivePersent() {
+//   const getInput = document.getElementById("input-bill").value; //x
+//   const finalOutput = (getInput * 5) / 100;
+//   document.getElementById("num").innerHTML = finalOutput;
+//   document.getElementById('input-bill').value = '0';
+// }
+// function tenPersent() {
+//   const getInput = document.getElementById("input-bill").value; //x
+//   const finalOutput = (getInput * 10) / 100;
+//   document.getElementById("num").innerHTML = finalOutput;
+//   document.getElementById('input-bill').value = '0';
+// }
+
+// function fifteenPersent() {
+//   const getInput = document.getElementById("input-bill").value; //x
+//   const finalOutput = (getInput * 15) / 100;
+//   document.getElementById("num").innerHTML = finalOutput;
+//   document.getElementById('input-bill').value = '0';
+// }
+
+// function twentyfivePersent() {
+//   const getInput = document.getElementById("input-bill").value; //x
+//   const finalOutput = (getInput * 25) / 100;
+//   document.getElementById("num").innerHTML = finalOutput;
+//   document.getElementById('input-bill').value = '0';
+// }
+
+// function thirtyfivePersent() {
+//   const getInput = document.getElementById("input-bill").value;
+//   const finalOutput = (getInput * 35) / 100;
+//   document.getElementById("num").innerHTML = finalOutput;
+//   document.getElementById('input-bill').value = '0';
+// }
+
+// function fifty() {
+//   const getInput = document.getElementById("input-bill").value;
+//   const finalOutput = (getInput * 50) / 100;
+//   document.getElementById("num").innerHTML = finalOutput;
+//   document.getElementById('input-bill').value = '0';
+// }
+// function sixtyfive() {
+//   const getInput = document.getElementById("input-bill").value;
+//   const finalOutput = (getInput * 65) / 100;
+//   document.getElementById("num").innerHTML = finalOutput;
+//   document.getElementById('input-bill').value = '0';
+// }
+// function eighty() {
+//   const getInput = document.getElementById("input-bill").value;
+//   const finalOutput = (getInput * 80) / 100;
+//   document.getElementById("num").innerHTML = finalOutput;
+//   document.getElementById('input-bill').value = '0';
+// }
+
+
+
+const tipsContainer = document.getElementById('buttons');
+const tipsRef = document.getElementsByClassName('tip');
+tipsContainer.addEventListener('click', (event) => {
+  const tipStrings = event.target.innerHTML;
+  const tipGetValues = parseFloat(tipStrings.replace('%', ''));
+  const input = document.getElementById('input-bill');
+  const inputValue = parseFloat(input.value);
+  if (!isNaN(inputValue)) {
+    const finalResult = (inputValue * tipGetValues) / 100;
+    document.getElementById('num').innerHTML = finalResult;
+  } else {
+    console.log('error bro');
   }
-  function tenPersent() {
-    const getInput = document.getElementById("input-bill").value; //x
-    const finalOutput = (getInput * 10) / 100;
-    document.getElementById("num").innerHTML = finalOutput;
-    document.getElementById('input-bill').value = '0';
-  }
-  
-  function fifteenPersent() {
-    const getInput = document.getElementById("input-bill").value; //x
-    const finalOutput = (getInput * 15) / 100;
-    document.getElementById("num").innerHTML = finalOutput;
-    document.getElementById('input-bill').value = '0';
-  }
-  
-  function twentyfivePersent() {
-    const getInput = document.getElementById("input-bill").value; //x
-    const finalOutput = (getInput * 25) / 100;
-    document.getElementById("num").innerHTML = finalOutput;
-    document.getElementById('input-bill').value = '0';
-  }
-  
-  function thirtyfivePersent() {
-    const getInput = document.getElementById("input-bill").value;
-    const finalOutput = (getInput * 35) / 100;
-    document.getElementById("num").innerHTML = finalOutput;
-    document.getElementById('input-bill').value = '0';
-  }
-  
-  function fifty() {
-    const getInput = document.getElementById("input-bill").value;
-    const finalOutput = (getInput * 50) / 100;
-    document.getElementById("num").innerHTML = finalOutput;
-    document.getElementById('input-bill').value = '0';
-  }
-  function sixtyfive() {
-    const getInput = document.getElementById("input-bill").value;
-    const finalOutput = (getInput * 65) / 100;
-    document.getElementById("num").innerHTML = finalOutput;
-    document.getElementById('input-bill').value = '0';
-  }
-  function eighty() {
-    const getInput = document.getElementById("input-bill").value;
-    const finalOutput = (getInput * 80) / 100;
-    document.getElementById("num").innerHTML = finalOutput;
-    document.getElementById('input-bill').value = '0';
-  }
-  
-  document.getElementById('reset').addEventListener('click',()=>{
-     document.getElementById('input-bill').value = '0';
-     document.getElementById('num').textContent = '0.00';
-  })
+})
+
+document.getElementById('reset').addEventListener('click', () => {
+  document.getElementById('input-bill').value = '0';
+  document.getElementById('num').textContent = '0.00';
+})
+
+// const tipValuesContainer = [];
+// for (let i = 0; i < tipsContainer.children.length; i++) {
+//   const tipStrings = tipsContainer.children[i].innerHTML
+//   const tipGetValues = parseFloat(tipStrings.replace('%', ''))
+//   tipValuesContainer.push(tipGetValues);
+// };
